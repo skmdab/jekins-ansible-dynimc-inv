fa94c978c849e8735bc1d0926d8b24a9206dfc16
skmdab <57156958+skmdab@users.noreply.github.com>
2023-06-22 13:58:04 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,6 +1,7 @@
 provider "aws"{
-    region = "ap-south-1"   
+region = "ap-south-1"   
 }
+
 resource "aws_instance" "AWS_EC2_INSTANCE"{
     ami = "ami-00f898fc5c0fb69d1"
     instance_type = "t2.micro"

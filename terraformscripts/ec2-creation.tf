15f0b476a09acfae34ca295b413c1426f9a2d869
skmdab <57156958+skmdab@users.noreply.github.com>
2023-06-22 14:30:12 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,7 +2,7 @@ provider "aws"{
 region = "ap-south-1"   
 }
 
-resource "aws_instance" "AWS_EC2_INSTANCE" {
+resource "aws_instance" "AWStomcat" {
 ami = "ami-00f898fc5c0fb69d1"
 instance_type = "t2.micro"
 key_name = "filinta"

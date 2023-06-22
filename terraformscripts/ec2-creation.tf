86663e01ab1a0c4e9d6059d912dc57004bc4f591
skmdab <57156958+skmdab@users.noreply.github.com>
2023-06-22 14:37:19 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,7 +3,7 @@ region = "ap-southeast-1"
 }
 
 resource "aws_instance" "AWStomcat" {
-ami = "ami-00f898fc5c0fb69d1"
+ami = "ami-02acda7aaa1f944e5"
 instance_type = "t2.micro"
 key_name = "filinta"
 security_groups = ["Filinta-public-subnet-1b"]
